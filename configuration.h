48857f105132a39e6c58b9e9ec1565ee05a0f107
Amir Iqbal <amir.iqbal@nu.edu.pk>
2020-10-22 08:46:53 +0500

    filter sizes 1,2,3

--- configuration.h
+++ configuration.h
@@ -82,8 +82,8 @@ const int num_negative_binary_operators = 2;
 const opType negative_binary_operators[] = {OPNAND, OPNOR};
 
 
-const int num_filter_sizes = 4;
-const int filter_sizes[] = {3, 5, 7, 9}; // filter sizes to be used
+const int num_filter_sizes = 3;
+const int filter_sizes[] = {1, 2, 3}; // filter sizes to be used
 const int max_filter_size = filter_sizes[num_filter_sizes-1];  // the last one should be maximum
 const bool allow_dilated_filters = true;
 
